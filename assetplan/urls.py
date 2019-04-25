6bc923d957dc43cce7c8223383286e80e043f69b
chirchir <chirchir7370@gmail.com>
2019-04-25 23:36:11 +0300

    created index page

--- assetplan/urls.py
+++ assetplan/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+	path('', include('secure.urls')),
     path('admin/', admin.site.urls),
 ]
 
