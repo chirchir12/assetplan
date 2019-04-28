040348b955cc91bbf5c13713e6d42ab6eaf0da5b
chirchir <chirchir7370@gmail.com>
2019-04-28 18:04:45 +0300

    added qoute model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+	fname    = models.CharField(max_length=250)
+	lname    = models.CharField(max_length=250)
+	phone    = models.CharField(max_length=100)
+	email    = models.EmailField(max_length=250, null=True, blank=True)
+	location = models.CharField(max_length=200)
+	subject  = models.CharField(max_length=255)
+	message  = models.TextField()
+
+	def __str__(self):
+		return self.subject
