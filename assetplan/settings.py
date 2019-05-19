9f3486696844b3b58488a94885f3e4ef886fab82
chirchir <chirchir7370@gmail.com>
2019-05-19 13:30:40 +0300

    timezone and database configurations

--- assetplan/settings.py
+++ assetplan/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'assetplan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'assetplan',
-        'USER':'assetsecurity',
-        'PASSWORD': 'Admin123',
+        'USER':'root',
+        'PASSWORD': 'Chirchir1.',
         'HOST':'localhost',
         'PORT': ''
 
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
